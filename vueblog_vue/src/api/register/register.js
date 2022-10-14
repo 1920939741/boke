e87fa5fba8b31bc71ba86e326a5654807f65c3fe
chengyiyong <c1920939741@163.com>
2022-10-14 10:00:19 +0800

    删除前端代码

--- vueblog_vue/src/api/register/register.js
+++
@@ -1,9 +0,0 @@
-import ajax from './ajax'
-
-// 定义BASE_URL
-// const BASE_URL = 'http://local:8081'
-const BASE_URL = '/api'
-
-
-export const registerUser = (data) => ajax(BASE_URL+'/registerUser',data, 'POST')
-
