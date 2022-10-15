7619489d2c8a92763d551e88624ce225602f837a
MS-202110152146\Administrator <3178501151@qq.com>
2022-10-15 15:15:50 +0800

    新加error结果集方法

--- vueblog_java/src/main/java/com/gyg/common/lang/Result.java
+++ vueblog_java/src/main/java/com/gyg/common/lang/Result.java
@@ -12,8 +12,23 @@ import java.io.Serializable;
 public class Result implements Serializable {
 
     private int code;   //200正常、非200异常
-    private String msg;     //提示信息
+    private String message;     //提示信息
     private Object data;    //返回数据
+
+    public Result(int code, String message, Object data){
+        this.code=code;
+        this.message=message;
+        this.data=data;
+    }
+
+    public Result() {
+
+    }
+
+    public Result(int code, String message) {
+        this.code=code;
+        this.message=message;
+    }
 
     public static Result success(Object data) {
         return success(200,"操作成功",data);
@@ -23,31 +38,49 @@ public class Result implements Serializable {
      * 消息返回方法
      *
      * @param code
-     * @param msg
+     * @param message
      * @param data
      * @return
      */
-    public static Result success(int code, String msg, Object data) {
+    public static Result success(int code, String message, Object data) {
         Result r = new Result();
         r.setCode(code);
-        r.setMsg(msg);
+        r.setMessage(message);
         r.setData(data);
         return r;
     }
 
 
-    public static Result fail(String msg) {
-        return fail(400,msg,null);
+    public static Result fail(String message) {
+        return fail(400,message,null);
     }
 
-    public static Result fail(String msg, Object data) {
-        return fail(400,msg,data);
+    public static Result fail(String message, Object data) {
+        return fail(400,message,data);
     }
 
-    public static Result fail(int code, String msg, Object data) {
+    /**
+     * 异常结果返回
+     * @param code
+     * @param message
+     * @return
+     */
+    public static Result error(int code,String message){
+        return new Result(code,message);
+    }
+
+    public static Result fail(int code, String message, Object data) {
         Result r = new Result();
         r.setCode(code);
-        r.setMsg(msg);
+        r.setMessage(message);
+        r.setData(data);
+        return r;
+    }
+
+    public static Result error(int code, String message, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMessage(message);
         r.setData(data);
         return r;
     }
