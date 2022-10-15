b23dbc787d6fd894de2f4bfe42033f84daa7d677
MS-202110152146\Administrator <3178501151@qq.com>
2022-10-15 15:15:12 +0800

    静态变量定义类

---
+++ vueblog_java/src/main/java/com/gyg/common/comtent/KeyContentCont.java
@@ -0,0 +1,14 @@
+package com.gyg.common.comtent;
+
+/**
+ * @author ：吉金武
+ * @ClassName KeyContentCont
+ * @date ：2022/10/14 - 21:53
+ * @version: V1.0
+ * @slogan: 天下风云出我辈，一入代码岁月催
+ * @description:
+ **/
+public class KeyContentCont {
+    public static final String EMAIL_CODE_PREFIX="emailCode";
+}
+
